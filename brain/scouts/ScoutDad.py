78af742b07f127fbb1022acf709dca0f627bd41c
reightenfels <reightenfels@mail.ru>
2025-10-16 16:34:19 +0300

    FIX ScoutDad

--- brain/scouts/ScoutDad.py
+++ brain/scouts/ScoutDad.py
@@ -1,5 +1,7 @@
-from ..Types import ScoutHead, Scout, Assets, Exchange
+from ..Types import ScoutHead, Scout, Assets, Exchange, Coin
 from asyncio import Queue
+import asyncio
+import ccxt.pro as ccxtpro
 
 '''
 Список Scouts'ов
@@ -19,10 +21,35 @@ class ScoutDad(ScoutHead):
             async with scout:
                 assets: Assets = await scout.watch_tickers()
                 exchange: Exchange = Exchange()
-                exchange.name = exchange_name
+                exchange.set_name(exchange_name)
 
                 yield (exchange, assets)
 
+    async def coin_list(self):
+        if (len(Coin.get_all_coin_names()) == 0):
+            print('All coin names is empty')
 
-scout_data = ScoutDad()
-scout_data.test()
+        result_dict = {}
+
+        for coin in Coin.get_all_coin_names():
+            coin_obj: Coin = Coin()
+            coin_obj.set_name(coin)
+
+            result_dict[coin_obj] = {}
+
+            for exchange_name, scout in self.scouts:
+                exchange_obj: Exchange = Exchange()
+                exchange_obj.set_name(exchange_name)
+
+                tickers = await scout.watch_tickers([coin])
+                for symbol, data in tickers.items():
+                    try:
+                        ask = float(data.get('ask') or 0.0)
+
+                        if (ask != 0.0):
+                            result_dict[coin_obj][exchange_obj] = ask
+
+                    except Exception:
+                        continue
+
+        return result_dict

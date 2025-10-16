593da6506f9c24a1e2168eb58adbb6a036aab60d
reightenfels <reightenfels@mail.ru>
2025-10-16 16:29:00 +0300

    FIX Coin

--- brain/Types/Scout.py
+++ brain/Types/Scout.py
@@ -8,7 +8,7 @@ class Scout(ABC):
     def get_intersection_coins (self) -> list:
         self.exchange.load_markets()
 
-        return list(set(Coin.get_all_coin_names_by_excange()).intersection(set(self.exchange.markets)))
+        return list(set(Coin.get_all_coin_names()).intersection(set(self.exchange.markets)))
 
     @abstractmethod
     async def watch_tickers(self, limit=10, params={}) -> AsyncIterator[Assets]:
